cc60a8237db0cb9bf72d0c404d5b0c516b0be93b
arunmeti2018 <arunameti2018@gmail.com>
2025-06-15 05:22:48 +0530

    Update package.json, config.js, and index.html

--- public/config.js
+++ public/config.js
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
+ 
+=======
 // Configuration file for API keys and other settings
 const config = {
   GEMINI_API_KEY: "AIzaSyCx1c_Lc4FjgexeQh3KnsG8QEr4KJKRA6Y", // Replace with your actual key if hardcoding for local dev
 };
 
 window.config = config;
+>>>>>>> 16e4b132b6e55c90e59b11f09a05dcfbb5a8ac37
